872792b15710f0123153dfc461eaff15cb83b594
bogdan <lakatosh@phystech.edu>
2020-09-28 13:14:14 +0300

    fixed splitting in argparse, so now can pass negative values

--- get_pt_from_surface_stat.py
+++ get_pt_from_surface_stat.py
@@ -61,9 +61,9 @@ def mk_array_from_hist(hist, bins):
 
 
 def convert_coor_ranges(dx, dy, dz):
-    dx = dx.split(';')
-    dy = dy.split(';')
-    dz = dz.split(';')
+    dx = dx.split(' ')
+    dy = dy.split(' ')
+    dz = dz.split(' ')
     for i in range(2):
         dx[i] = float(dx[i])
         dy[i] = float(dy[i])
@@ -73,18 +73,18 @@ def convert_coor_ranges(dx, dy, dz):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s'  ,'--species'     ,action='store', default='H+;H2+;H3+', help='list of particles I am interested in [def = "H+;H2+;H3+"]')
+    parser.add_argument('-s'  ,'--species'     ,action='store', default='H+ H2+ H3+', help='list of particles I am interested in [def = "H+ H2+ H3+"]')
     parser.add_argument('-F', '--filter'  , action='store_true', default=False, help='True/False Turn on filter by coordinates [def = False]')
-    parser.add_argument('-dx' ,'--dx'     ,action='store', default='-7.0;7.0', help='x interval [def = "-7.0;7.0"]')
-    parser.add_argument('-dy' ,'--dy'     ,action='store', default='7.5;9.7', help='y interval [def = "7.5;9.7"]')
-    parser.add_argument('-dz' ,'--dz'     ,action='store', default='164.0;165.6', help='z interval [def = "164.0;165.6"]')
+    parser.add_argument('-dx' ,'--dx'     ,action='store', default='-7.0 7.0', help='x interval [def = "-7.0 7.0"]')
+    parser.add_argument('-dy' ,'--dy'     ,action='store', default='7.5 9.7', help='y interval [def = "7.5 9.7"]')
+    parser.add_argument('-dz' ,'--dz'     ,action='store', default='164.0 165.6', help='z interval [def = "164.0 165.6"]')
     parser.add_argument('-p'  ,'--path'   , action='store', default='particles', help="path to particle dataset in h5 [default = 'particles']") 
     parser.add_argument('-o' ,'--out_file'     ,action='store',default='collected_pt', help='name for output file name [def = collected_pt]')
     parser.add_argument('files', nargs='+')
     args = parser.parse_args()
     
     
-    pt_list = args.species.split(';')
+    pt_list = args.species.split(' ')
     dx, dy, dz = convert_coor_ranges(args.dx, args.dy, args.dz)
     path = args.path.split('/')
     
